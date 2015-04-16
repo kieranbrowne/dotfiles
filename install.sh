c4e472827db78fa4cd7acca5b91762ce2ea8ecc9
kieranbrowne <kierantbrowne@gmail.com>
2015-04-16 19:34:29 +1000

    safer, with testing before most actions

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ############################
-# .make.sh
+# install.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
@@ -12,20 +12,14 @@ files="bashrc vimrc zshrc profile oh-my-zsh" # list of files/folders to symlink 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
@@ -36,33 +30,35 @@ install_vim () {
     mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
     cd ~/.vim/bundle
-    git clone https://github.com/scrooloose/nerdtree.git
+    if [ ! -d nerdtree/ ]; then
+        git clone https://github.com/scrooloose/nerdtree.git
+    fi
 }
 
 install_zsh () {
 # Test to see if zshell is installed. If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+        if [ ! -d $dir/oh-my-zsh/ ]; then
+            git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+        fi
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
+        fi
+        else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            sudo apt-get install zsh
+            install_zsh
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
     fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-echo "Please install zsh, then re-run this script!"
-        exit
-fi
-fi
 }
 
 install_zsh
@@ -76,8 +72,8 @@ cd ~
 ln -s $dir/bin
 # set solarized colorscheme
 cd ~
-wget --no-check-certificate -O .dircolors https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
-eval `dircolors ~/.dircolors`
-git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
-cd gnome-terminal-colors-solarized
-./set_dark.sh
+wget -q --no-check-certificate -O .dircolors https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+if [ ! -d ~/gnome-terminal-colors-solarized/ ]; then
+    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+fi
+./gnome-terminal-colors-solarized/set_dark.sh
